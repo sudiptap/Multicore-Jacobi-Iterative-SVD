723d22a5211bbf5d386102ff6369023dfefe20c7
Soumitra Pal <soumitrakp@gmail.com>
2016-06-09 17:43:30 -0400

    now optimal_pair multicore is giving 2.5 scaling using 4 threads

--- src/jacobi_gsl_optimal_pair_multicore.hpp
+++ src/jacobi_gsl_optimal_pair_multicore.hpp
@@ -25,8 +25,8 @@ class JacobiGSLOptimalPairMulticore : public SVDecomposer<JacobiGSLOptimalPairMu
        long indx_sz = populate_indices(indices, A, S, M, N);
         std::sort(indices.begin(), indices.begin()+indx_sz, sort_desc);
         if (get<2>(indices[0]) <= tolerance) break;
-        //size_t pivot_to_use = min(indx_sz, pivot_count / params.top_frac);
-        while (indx_sz > 10*params.num_threads) {
+        indx_sz = min(indx_sz, (long)pivot_count / params.top_frac);
+        while (indx_sz > 0) {
           size_t pivot_used = 0;
           fill_n(begin(visited), N, 0);
           for (long idx=0; idx < indx_sz; ) {
@@ -42,27 +42,30 @@ class JacobiGSLOptimalPairMulticore : public SVDecomposer<JacobiGSLOptimalPairMu
             }
           }
           //cout << "Number of pivots used in sweep " << sweep << " = " << pivot_used << endl;
+          if (pivot_used > 5*params.num_threads) {
 #pragma omp parallel for
-          for(size_t idx=0; idx< pivot_used; idx++) {
-            size_t j = get<0>(ind_pivots[idx]);
-            size_t k = get<1>(ind_pivots[idx]);
-            double cosine, sine;
-            if (needs_update(j, k, cosine, sine)) {
-              do_update(j,k,cosine,sine);
-              update_count++;
+            for(size_t idx=0; idx< pivot_used; idx++) {
+              size_t j = get<0>(ind_pivots[idx]);
+              size_t k = get<1>(ind_pivots[idx]);
+              double cosine, sine;
+              if (needs_update(j, k, cosine, sine)) {
+                do_update(j,k,cosine,sine);
+#pragma omp atomic
+                update_count++;
+              }
             }
-          }
-          for(long idx=0; idx< indx_sz; idx++) {
-            size_t j = get<0>(indices[idx]);
-            size_t k = get<1>(indices[idx]);
-            double cosine, sine;
-            if (needs_update(j, k, cosine, sine)) {
-              do_update(j,k,cosine,sine);
-              update_count++;
+          } else {
+            for(size_t idx=0; idx< pivot_used; idx++) {
+              size_t j = get<0>(ind_pivots[idx]);
+              size_t k = get<1>(ind_pivots[idx]);
+              double cosine, sine;
+              if (needs_update(j, k, cosine, sine)) {
+                do_update(j,k,cosine,sine);
+                update_count++;
+              }
             }
           }
         }
-
         sweep++;
       }
       return GSL_SUCCESS;
